6c1547c199cedc667e036d5c7555075e7f085634
zooky12 <zooky12@gmail.com>
2024-01-20 02:32:49 +0100

    wip

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const noiseLevel = 200;
 const noiseRadius = 1.5;
-const increment = 0.005;
+const increment = 0.009;
 
 let theta = 0;
 let first = true;
@@ -25,7 +25,7 @@ const sketch_graph = (p) => {
 
   p.draw = function () {
     p.background(255);
-    numbers = createArray(p,theta);
+    numbers = createArray(p,theta,[50,50,50]);
     numbers2 = shiftArray(numbers,numbers.length / 2);
 
     p.stroke(...color_numbers1);p.strokeWeight(1);renderNumbers(numbers, p);
@@ -78,29 +78,30 @@ const sketch_earth = (p) => {
 };
 
 const sketch_texture = (p) => {
+  p.preload = function () {
+    p.img = p.loadImage("assets/globe.jpg");
+  };
   p.setup = function () {
     p.amount = p.TAU / increment;
     p.createCanvas(p.amount, p.amount, p.WEBGL);
+    p.translate(-p.width / 2, -p.height / 2);
     p.background(0);
-    numbers = createArray(p,theta);
-    numbers2 = shiftArray(numbers,numbers.length / 2);
-    console.log(numbers);
+    //p.image(p.img,0,0,p.amount, p.amount);
+    //numbers = createArray(p,theta,[50,50,(50+p.frameCount)]);
+    //numbers2 = shiftArray(numbers,numbers.length / 2);
     
   };
 
   p.draw = function () {
-    
-    p.stroke(255);
+    p.background(0);
+    p.stroke(0);
     p.strokeWeight(2);
-    p.noFill();
-    p.circle(20, 20);
-    numbers = createArray(p,theta);
+    numbers = createArray(p,theta,[50,50,50]);
     numbers2 = shiftArray(numbers,numbers.length / 2);
-    if(first){
-      //renderIntersectionTextureF(numbers,numbers2,p);
-      first = false;
-    }
-    renderIntersectionTexture(numbers, numbers2, p);
+
+    renderIntersectionTextureF(p);
+    //p.noLoop();
+    //renderIntersectionTexture(numbers, numbers2, p);
   };
 };
 
@@ -109,7 +110,7 @@ const sketch_texture = (p) => {
 
 
 /** DRAW */
-new p5(sketch_graph);
+//new p5(sketch_graph);
 //new p5(sketch_earth);
 new p5(sketch_texture);
 
@@ -121,13 +122,24 @@ new p5(sketch_texture);
 
 /** CREATORS */
 
-function createArray(p,t){
-  const pos = [50,50,50]
+function createArray(p,t,pos){
+  //const pos = [50,50,50]
   const l_array = []
   for (let phi = 0; phi < p.TAU; phi += increment) {
     const x = noiseRadius * p.sin(phi) * p.cos(t);
     const y = noiseRadius * p.sin(phi) * p.sin(t);
     const z = noiseRadius * p.cos(phi);
+    l_array.push(noiseLevel * p.noise(x + pos[0], y + pos[1], z + pos[2]));
+  }
+  return l_array
+}
+function createArray2(p,t,pos){
+  //const pos = [50,50,50]
+  const l_array = []
+  for (let phi = 0; phi < p.TAU; phi += increment) {
+    const x = noiseRadius * p.sin(t) * p.cos(phi);
+    const y = noiseRadius * p.sin(t) * p.sin(phi);
+    const z = noiseRadius * p.cos(t);
     l_array.push(noiseLevel * p.noise(x + pos[0], y + pos[1], z + pos[2]));
   }
   return l_array
@@ -152,45 +164,50 @@ function renderIntersection(num, num2, p) {
   }
   p.pop();
 }
-function renderIntersectionTexture(num, num2, p) {
+function renderIntersectionTexture(num, p, x) {
   p.stroke(255);
-  p.strokeWeight(5);
+  p.strokeWeight(3);
   p.push();
   p.translate(-p.width / 2, -p.height / 2);
-  for (let x = 0; x < num.length - 1; x++) {
+  for (let l_x = 0; l_x < num.length - 1; l_x++) {
+    const l_y = ((l_x+Math.floor(num.length/2))%num.length)
+    const l_y2 = ((l_x+1+Math.floor(num.length/2))%num.length)
+    if (num[l_x] == num[l_y]) renderITPoints(x, l_x,p);
+    if (num[l_x] > num[l_y] && num[l_x + 1] < num[l_y2]) renderITPoints(x, l_x/num.length*p.amount,p);
+    if (num[l_x] < num[l_y] && num[l_x + 1] > num[l_y2]) renderITPoints(x, l_x/num.length*p.amount,p);
     
-    if (num[x] == num2[x]) renderITPoints(p.frameCount, x);
-    if (num[x] > num2[x] && num[x + 1] < num2[x + 1]) renderITPoints(p.frameCount, x/num.length*p.amount,p);
-    if (num[x] < num2[x] && num[x + 1] > num2[x + 1]) renderITPoints(p.frameCount, x/num.length*p.amount,p);
+  }
+  p.pop();
+}
+function renderIntersectionTexture2(num, p, x) {
+  p.stroke(255);
+  p.strokeWeight(3);
+  p.push();
+  p.translate(-p.width / 2, -p.height / 2);
+  for (let l_x = 0; l_x < num.length - 1; l_x++) {
+    const l_y = ((l_x+Math.floor(num.length/2))%num.length)
+    const l_y2 = ((l_x+1+Math.floor(num.length/2))%num.length)
+    if (num[l_x] == num[l_y]) renderITPoints(l_x*2, x*2,p);
+    if (num[l_x] > num[l_y] && num[l_x + 1] < num[l_y2]) renderITPoints(l_x/num.length*p.amount*2,x*2,p);
+    if (num[l_x] < num[l_y] && num[l_x + 1] > num[l_y2]) renderITPoints(l_x/num.length*p.amount*2,x*2,p);
     
   }
   p.pop();
 }
 function renderITPoints(x,y,p) {
-  if(!p)return;
   p.point(x*2, y*2);
-  p.point((x*2+p.amount/2)%p.amount, p.amount-y*2);
+  //p.point((x*2+p.amount/2)%p.amount, p.amount-y*2);
 }
 
-function renderIntersectionTextureF(num, num2, p) {
-  console.log(p);
-  if(!p)return;
-  for(let i = 0; i<p.TAU; i+=0.001){
-    num=createArray(p,i);
-    num2=shiftArray(num,num.length);
-    renderIntersectionTexture(num, num2, p)
+function renderIntersectionTextureF(p) {
+  for(let i = 0; i<p.TAU; i+=0.008){
+    let x = p.width * i / p.TAU / 2;
+    let num=createArray(p,i,[50,50,50+p.frameCount/50]);
+    //const num2=shiftArray(num,num.length/2);
+    renderIntersectionTexture(num, p, x);
+    num=createArray2(p,i,[50,50,50+p.frameCount/50]);
+    renderIntersectionTexture2(num, p, x);
   }
-}
-
-function renderFullIntersectionTexture(p) { // TODO
-  let num = [];
-  let num2 = [];
-  for(let i = 0; i<p.TAU; i+=0.1){
-    num=createArray(p,i);
-    num2=shiftArray(num,num.length);
-    renderIntersectionTexture(num, num2, p)
-  }
-  p.pop();
 }
 
 /** OTHER */
