b35d01cbb0cfc541ab8eff1ad1a5392d83bd7a55
zooky12 <zooky12@gmail.com>
2024-01-26 17:00:07 +0100

    optimitzacio v1

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
-const noiseLevel = 200;
-const noiseRadius = 1.5;
-const increment = 0.009;
+const noiseLevel = 3;
+const noiseRadius = 1.3;
+const increment = 0.01;
+const peixet = 0.001;
 
 let theta = 0;
 let first = true;
@@ -105,6 +106,39 @@ const sketch_texture = (p) => {
   };
 };
 
+const sketch_texture_points = (p) => {
+  let points = [];
+  p.preload = function () {
+    p.img = p.loadImage("assets/globe.jpg");
+  };
+  p.setup = function () {
+    p.amount = p.TAU / increment;
+    console.log(p.TAU / increment);
+    p.createCanvas(p.amount, p.amount, p.WEBGL);
+    p.translate(-p.width / 2, -p.height / 2);
+    p.background(0);
+    //p.image(p.img,0,0,p.amount, p.amount);
+    //numbers = createArray(p,theta,[50,50,(50+p.frameCount)]);
+    //numbers2 = shiftArray(numbers,numbers.length / 2);
+
+    points = createPoints(p,[50,50,50]);
+    //console.log(createPoints(p,[50,50,50]));
+    
+  };
+
+  p.draw = function () {
+    console.log("A");
+    p.background(0);
+    p.stroke(255);
+    p.strokeWeight(2);
+    renderPointArray(createPoints(p,[50,50,50+p.frameCount*0.005]),p);
+    //renderAndCreatePoints(p,[50,50,50+p.frameCount*0.01]);
+
+    //p.noLoop();
+    //renderIntersectionTexture(numbers, numbers2, p);
+  };
+};
+
 
 /** -------------------------- */
 
@@ -112,7 +146,8 @@ const sketch_texture = (p) => {
 /** DRAW */
 //new p5(sketch_graph);
 //new p5(sketch_earth);
-new p5(sketch_texture);
+//new p5(sketch_texture);
+new p5(sketch_texture_points);
 
 
 
@@ -122,6 +157,61 @@ new p5(sketch_texture);
 
 /** CREATORS */
 
+function createPoints(p,pos){
+  const l_points = [];
+  const l_increment_t = increment;
+  let l_increment_phi = increment;
+
+  let last_point_original = 0;
+  let last_point_inverse = 0;
+  let point_original = 0;
+  let point_inverse = 0;
+
+  for(let t = l_increment_t; t < p.TAU; t += l_increment_t){
+    phi = 0
+    let [last_point_original,last_point_inverse] = createPointPair(t,phi,pos,p);
+    for (let phi = l_increment_phi; phi < p.TAU/2; phi += l_increment_phi) {
+      let [point_original,point_inverse] = createPointPair(t,phi,pos,p);
+      
+      if (isIntersection(last_point_original,point_original,last_point_inverse,point_inverse)) {
+        l_points.push([t * 1 / increment,phi * 2 / increment ])
+      }
+      [last_point_original,last_point_inverse] = [point_original,point_inverse];
+    }
+  }
+  for(let phi = l_increment_t; phi < p.TAU/2; phi += l_increment_t){
+    t = 0;
+    let [last_point_original,last_point_inverse] = createPointPair(t,phi,pos,p);
+
+    for (let t = l_increment_phi; t < p.TAU; t += l_increment_phi) {
+      let [point_original,point_inverse] = createPointPair(t,phi,pos,p);
+      
+      if (isIntersection(last_point_original,point_original,last_point_inverse,point_inverse)) {
+        l_points.push([t * 1 / increment,phi * 2 / increment ])
+      }
+      [last_point_original,last_point_inverse] = [point_original,point_inverse];
+    }
+  }
+  return l_points
+}
+function createPointPair(t,phi,pos,p){
+  const sp = p.sin(phi);
+  const st = p.sin(t);
+  const spp = p.sin(phi+p.TAU/2);
+  const cp = p.cos(phi);
+  const ct = p.cos(t);
+  
+  const cpp = p.cos(phi+p.TAU/2)
+  const point_original = noiseLevel * p.noise( 
+                        noiseRadius * sp * ct + pos[0],
+                        noiseRadius * sp * st + pos[1],
+                        noiseRadius * cp + pos[2]);
+  const point_inverse = noiseLevel * p.noise(
+                        noiseRadius * spp * ct + pos[0],
+                        noiseRadius * spp * ct + pos[1],
+                        noiseRadius * cpp + pos[2]);
+  return [point_original,point_inverse];
+}
 function createArray(p,t,pos){
   //const pos = [50,50,50]
   const l_array = []
@@ -164,6 +254,49 @@ function renderIntersection(num, num2, p) {
   }
   p.pop();
 }
+function renderPointArray(points, p) {
+  p.push();
+  p.translate(-p.width / 2, -p.height / 2);
+  for (let x = 0; x < points.length; x++) {
+    p.point(points[x][0], points[x][1]);
+  }
+  p.pop();
+}
+function renderAndCreatePoints(p,pos){
+  const l_increment_t = increment;
+  let l_increment_phi = increment;
+
+  let last_point_original = 0;
+  let last_point_inverse = 0;
+  let point_original = 0;
+  let point_inverse = 0;
+
+  for(let t = l_increment_t; t < p.TAU; t += l_increment_t){
+    phi = 0
+    let [last_point_original,last_point_inverse] = createPointPair(t,phi,pos,p);
+    for (let phi = l_increment_phi; phi < p.TAU/2; phi += l_increment_phi) {
+      let [point_original,point_inverse] = createPointPair(t,phi,pos,p);
+      
+      if (isIntersection(last_point_original,point_original,last_point_inverse,point_inverse)) {
+        p.point([t * 1 / increment,phi * 2 / increment ]);
+      }
+      [last_point_original,last_point_inverse] = [point_original,point_inverse];
+    }
+  }
+  for(let phi = l_increment_t; phi < p.TAU/2; phi += l_increment_t){
+    t = 0;
+    let [last_point_original,last_point_inverse] = createPointPair(t,phi,pos,p);
+
+    for (let t = l_increment_phi; t < p.TAU; t += l_increment_phi) {
+      let [point_original,point_inverse] = createPointPair(t,phi,pos,p);
+      
+      if (isIntersection(last_point_original,point_original,last_point_inverse,point_inverse)) {
+        p.point([t * 1 / increment,phi * 2 / increment ])
+      }
+      [last_point_original,last_point_inverse] = [point_original,point_inverse];
+    }
+  }
+}
 function renderIntersectionTexture(num, p, x) {
   p.stroke(255);
   p.strokeWeight(3);
@@ -198,7 +331,6 @@ function renderITPoints(x,y,p) {
   p.point(x*2, y*2);
   //p.point((x*2+p.amount/2)%p.amount, p.amount-y*2);
 }
-
 function renderIntersectionTextureF(p) {
   for(let i = 0; i<p.TAU; i+=0.008){
     let x = p.width * i / p.TAU / 2;
@@ -212,6 +344,12 @@ function renderIntersectionTextureF(p) {
 
 /** OTHER */
 
+function isIntersection(x,x2,y,y2){
+  if (x == y) return true;
+  if (x+peixet > y-peixet && x2-peixet < y2+peixet) return true;
+  if (x-peixet < y+peixet && x2+peixet > y2-peixet) return true;
+  return false;
+}
 function drawGraph(num, p) {
   for (let i = 0; i < num.length; i++) {
     p.vertex(i, num[i]);
